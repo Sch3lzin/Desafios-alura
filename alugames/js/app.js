462081c0fe8d08a65c244b27b0a895a6f42318cc
Daniel Schefer <danielschefer2007@hotmail.com>
2025-05-02 01:00:32 -0300

    Upgrade in alugames/app.js

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -4,15 +4,11 @@ function alterarStatus(id) {
 
     if (btn.classList.contains('dashboard__item__button--return')) {
         btn.classList.remove('dashboard__item__button--return');
+        img.classList.remove('dashboard__item__img--rented');
         btn.innerText = "Alugar";
     } else {
         btn.classList.add('dashboard__item__button--return');
-        btn.innerText = "Devolver";
-    }
-
-    if (img.classList.contains('dashboard__item__img--rented')) {
-        img.classList.remove('dashboard__item__img--rented');
-    } else {
         img.classList.add('dashboard__item__img--rented');
+        btn.innerText = "Devolver";
     }
 }
