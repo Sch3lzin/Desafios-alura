c19f3b20c076503f6eef7bd2c0c47f6f90c590a2
Daniel Schefer <danielschefer2007@hotmail.com>
2025-05-02 13:12:55 -0300

    Upgrade in Alugames

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -1,14 +1,35 @@
+let jogosAlugados = [];
+
 function alterarStatus(id) {
     let btn = document.getElementById('btn-' + id);
     let img = document.getElementById('img-' + id);
+
+    let acao = btn.classList.contains('dashboard__item__button--return') ? "devolver" : "alugar";
+    let confirmar = confirm(`Tem certeza que deseja ${acao} este item?`);
+
+    if (!confirmar) {
+        return;
+    }
+
+    let nomeJogo = document.getElementById('name-' + id).innerText;
 
     if (btn.classList.contains('dashboard__item__button--return')) {
         btn.classList.remove('dashboard__item__button--return');
         img.classList.remove('dashboard__item__img--rented');
         btn.innerText = "Alugar";
+
+        let index = jogosAlugados.indexOf(nomeJogo);
+        if (index !== -1) {
+            jogosAlugados.splice(index, 1);
+        }
+
     } else {
         btn.classList.add('dashboard__item__button--return');
         img.classList.add('dashboard__item__img--rented');
         btn.innerText = "Devolver";
+
+        jogosAlugados.push(nomeJogo);
     }
+
+    console.log(`Jogos alugados: ${jogosAlugados.join(', ')}`);
 }
